d0830fc4b1795e3993808f03f71030dd6e5d67c5
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2023-10-04 11:57:49 +0000

    Handle device entity deletion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const env = require('env-var')
 const {
   generateThingDescription
 } = require('./lib/thing_description_template')
-const { updateThing } = require('./lib/thing_registry')
+const { updateThing, deleteThing } = require('./lib/thing_registry')
 
 const Port = env.get('PORT').default('3000').required(true).asPortNumber()
 const app = express()
@@ -22,27 +22,50 @@ app.post('/', async (req, res) => {
   const tenantName = req.headers['x-tenant-name']
   const credentials = req.headers['x-credentials']
   const credentialsType = req.headers['x-credentials-type']
+  const messageType = req.headers['x-message-type']
   // const tenantId = req.headers['x-tenant-id']
 
-  if (
-    thingModelUrl === undefined ||
-    deviceId === undefined ||
-    tenantName === undefined ||
-    credentials === undefined ||
-    credentialsType === undefined
-  ) {
+  if (messageType === undefined) {
     return res.status(400).send('Bad Request')
   }
 
   try {
-    const thingDescription = await generateThingDescription({
-      deviceId,
-      thingModelUrl,
-      tenantName,
-      credentials,
-      credentialsType
-    })
-    await updateThing(tenantName, thingDescription)
+    if (messageType === 'ATTRIBUTES_UPDATED') {
+      if (
+        thingModelUrl === undefined ||
+        deviceId === undefined ||
+        tenantName === undefined ||
+        credentials === undefined ||
+        credentialsType === undefined
+      ) {
+        return res.status(400).send('Bad Request')
+      }
+
+      const thingDescription = await generateThingDescription({
+        deviceId,
+        thingModelUrl,
+        tenantName,
+        credentials,
+        credentialsType
+      })
+      
+      await updateThing(tenantName, thingDescription)
+    } else if (messageType === 'ATTRIBUTES_DELETED') {
+      if (deviceId === undefined || tenantName === undefined) {
+        return res.status(400).send('Bad Request')
+      }
+
+      await deleteThing(tenantName, `uri:uuid:${deviceId}`)
+    } else if (messageType === 'ENTITY_DELETED') {
+      if (tenantName === undefined) {
+        return res.status(400).send('Bad Request')
+      }
+
+      if (req.body.id.entityType === 'DEVICE') {
+        await deleteThing(tenantName, `uri:uuid:${req.body.id.id}`)
+      }
+    }
+
     res.send('OK')
   } catch (e) {
     return res.status(500).send('Internal Server Error')
